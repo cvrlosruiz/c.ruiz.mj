b87cb54538c7f8fbe269e73bd55da9e93d579896
Carlos Ruiz <carlosruiz397@yahoo.com>
2024-03-19 10:00:22 -0400

    ejercicio1 assesment

---
+++ LABS C++/promedioexam.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+   double exam1, exam2, exam3, exam4;
+
+   cout <<"Ingrese la nota del primer examen:";
+    cin >> exam1;
+
+    cout <<"Ingrese la nota del segundo examen:";
+    cin >> exam2;
+
+    cout <<"Ingrese la nota del tercer examen:";
+    cin >> exam3;
+
+    cout <<"Ingrese la nota del cuarto examen:";
+    cin >> exam4;
+
+    double resultado = exam1 + exam2 + exam3 + exam4;
+
+    double promedio = resultado /4;
+
+    cout<<"Mi promedio es:" <<promedio << endl;
+
+
+
+    return 0;
+}
